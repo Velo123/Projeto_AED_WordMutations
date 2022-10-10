5939cf2791ae53bc6492f068a2d5ec390472c777
Velo123 <miguelsvelo@gmail.com>
2022-10-10 14:25:00 +0100

    updates

--- Main.c
+++ Main.c
@@ -12,7 +12,6 @@ int main(int argc, char *argv[]){
 
     ordenardict(dict);
 
-
     FILE *ifp = fopen(argv[2],"r");
     if(ifp==NULL){
         sdfree(dict);
@@ -26,26 +25,37 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
     
+    int t1=0, t2=0;
+
     prob p;
     while (!feof(ifp))
     {
         rprob(ifp,&p);
         if (p.mod==1)
         {
-            fprintf(ofp,"%s %d\n\n",p.pal1,getsizetotal(dict,strlen(p.pal1)-2));
+            if((t1=getwpos(dict,p.pal1))==-1){
+                fprintf(ofp,"%s %s %d\n\n",p.pal1,p.pal2,p.mod);
+            }
+            else{   
+                fprintf(ofp,"%s %d\n\n",p.pal1,getsizetotal(dict,strlen(p.pal1)-2));
+            }
         }
         else if(p.mod==2)
         {
-            fprintf(ofp,"%s %d\n%s %d\n\n",p.pal1,getwpos(dict,p.pal1),p.pal2,getwpos(dict,p.pal2));
+            if(((t1=getwpos(dict,p.pal1))==-1) || ((t2=getwpos(dict,p.pal2))==-1)){
+                fprintf(ofp,"%s %s %d\n\n",p.pal1,p.pal2,p.mod);
+            }
+            else{   
+                fprintf(ofp,"%s %d\n%s %d\n\n",p.pal1,t1,p.pal2,t2);
+            }
         }
         else
         {
-            fprintf(ofp,"%s %s %d\n",p.pal1,p.pal2,p.mod);
+            fprintf(ofp,"%s %s %d\n\n",p.pal1,p.pal2,p.mod);
         }
     }
     fclose(ifp);
     fclose(ofp);
     sdfree(dict);
-    
     return 0;
 }
--- output.c
+++ output.c
@@ -1,14 +1,15 @@
 #include  "output.h"
 
 FILE *openfile(char *argv[]){
-    char ext[]=".stats";
-    char* fout = argv[2];
-    char* temp = strstr(fout,".pals");
-    
-    memmove(temp,ext,7);
-
-    FILE *fp=fopen(fout,"w+");
-    
+    char *fout=(char*)malloc((strlen(argv[2])+2)*sizeof(char));
+    if (fout==NULL)
+        return 0;
+    strcpy(fout,argv[2]);
+    fout[strlen(argv[2])-5]='\0';
+    strcat(fout,".stats");
+    FILE *fp=fopen(fout,"w");
+    free(fout);
+        
     return fp;
 }
 
